2cfa139a515dfe9bb4e2feb7e406f85d9664c3c8
RishiDeKayne <rishidek@hotmail.com>
2019-04-01 17:00:01 +0200

    Update LinkageMap_Chromosome_Syntey_circosplot.R

--- LinkageMap_Chromosome_Syntey_circosplot.R
+++ LinkageMap_Chromosome_Syntey_circosplot.R
@@ -27,13 +27,12 @@
 #       - bsub -n10 -W 4:00 -R "rusage[mem=3000]" "module load java gdc bwa/0.7.17; bwa mem -t 10 module load java gdc bwa/0.7.17; bwa mem -t 10 chromosome_level_assembly.fasta SpeciesAveragedLinkageMap.fasta > SA_linkagemap_FalconPhaseMapped.sam"
 #       - awk '{ if($5 > 30) {print}}' SA_linkagemap_FalconPhaseMapped.sam > SA_linkagemap_FalconPhaseMapped_Filtered.txt
 
-#       - this was then checked over in excel and the column with mapping location separated by scaffold number and bp location e.g. - check columns carefully first, there are some gaps in the header line
+#       - this was then checked over and the columns with markers were split into rad locus id and linkage group and the mapping location separated by scaffold number and bp location e.g. - check columns fit the example carefully - it is not just sam output!
 
 #e.g.
-#@PG			ID:bwa	PN:bwa		VN:0.7.17-r1188	CL:bwa	mem	#NAME?	10	/cluster/project/gdc/shared/p298/Rishi/PostPhaseScaffoldingGenomes/WF_Canu.chr.fasta	/cluster/project/gdc/shared/p298/Rishi/LinkageMap/SpeciesAveragedMap.fasta						
-#consensus_42108	SA10	0	0	PGA	scaffold21	16735146	60	90M	*	0	0	CACTCTCTCTCCTTCTCCCTCCTTTCCTCTCTATGTCCCAGAAAGCCTTCAAAGATGACCCGCAGAGCAGAGCGGTGGTATGATCCTGCA	*	NM:i:0	MD:Z:90	AS:i:90	XS:i:21	
-#consensus_27024	SA10	2.3	0	PGA	scaffold21	6158036	60	90M	*	0	0	TGCAGGGTTCAAATGCAACCACTTGGGCAGCCTGGTCTCATAGGCTTGACGTAACATAGAAAATGTAAATCTGGGACACTCAAATTAGTA	*	NM:i:0	MD:Z:90	AS:i:90	XS:i:23	
-#consensus_7502	SA10	2.63	16	PGA	scaffold21	26075329	60	90M	*	0	0	GCAGCTGCTGCTTGTGGGCCTCCTTAATTGCTTGCAGCTGGCGTTGGAGCAGCAGCTCTGATCTCAGGCCATCCTCCAGGAAAGCCTGCA	*	NM:i:1	MD:Z:25C64	AS:i:85	XS:i:20	
+#X.PG	X	X.1	ID.bwa	PN.bwa	X.2	VN.0.7.17.r1188	CL.bwa	mem	X.NAME.	X10	X.cluster.project.gdc.shared.p298.Rishi.PostPhaseScaffoldingGenomes.WF_Canu.chr.fasta	X.cluster.project.gdc.shared.p298.Rishi.LinkageMap.SpeciesAveragedMap.fasta	X.3	X.4	X.5	X.6	X.7
+#consensus_42108	SA10	0	0	PGA	scaffold21	16735146	60	90M	*	0	0	CACTCTCTCTCCTTCTCCCTCCTTTCCTCTCTATGTCCCAGAAAGCCTTCAAAGATGACCCGCAGAGCAGAGCGGTGGTATGATCCTGCA	*	NM:i:0	MD:Z:90	AS:i:90	XS:i:21
+#consensus_27024	SA10	2.3	0	PGA	scaffold21	6158036	60	90M	*	0	0	TGCAGGGTTCAAATGCAACCACTTGGGCAGCCTGGTCTCATAGGCTTGACGTAACATAGAAAATGTAAATCTGGGACACTCAAATTAGTA	*	NM:i:0	MD:Z:90	AS:i:90	XS:i:23
 
 #for extra information about plotting check out the circlize manual: https://jokergoo.github.io/circlize_book/book/ 
 
